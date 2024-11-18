196333eedbf05f7724aad8fa2b727b2aef3f58f9
Julien QUENTIN <jqntn88@gmail.com>
2024-11-19 00:17:04 +0100

    try win32 createwindow

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
 md build
-tcc main.c -o build/main.exe
+tcc main.c -o build/main.exe -luser32
--- main.c
+++ main.c
@@ -1,9 +1,30 @@
+#define VC_EXTRALEAN
+#define WIN32_LEAN_AND_MEAN
+
+#include <Windows.h>
+
 #include <stdio.h>
+#include <stdlib.h>
 
 int
 main(void)
 {
+  auto W = CreateWindow(
+    L"Static", L" ", WS_POPUP | WS_SIZEBOX | SS_NOTIFY, 0, 0, 0, 0, 0, 0, 0, 0);
+
+  auto H = GetDC(W);
+  ShowWindow(W, 3);
+
+  MSG M;
+
+  while (1) {
+    if (PeekMessage(&M, 0, 0, 0, 1))
+      DispatchMessage(&M);
+  }
+
+  /**/
+
   puts("Hello, World!");
 
-  return 0;
+  return EXIT_SUCCESS;
 }
